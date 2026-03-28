15f11f3ca61864267dca94d3f969ae0a6bc31f0c
Ozelot Vanilla <ozelotvanilla@gmail.com>
2026-03-28 23:06:59 +0900

    update - `0.1.1.0000`

--- Formula/jjdev.rb
+++ Formula/jjdev.rb
@@ -1,8 +1,8 @@
 class Jjdev < Formula
     desc "tmux helper for jj development workflow"
     homepage "https://github.com/OzelotVanilla/jjdev"
-    url "https://github.com/OzelotVanilla/jjdev/archive/refs/tags/v0.1.0.0000.tar.gz"
-    sha256 "efa8a0a012ce41ac8a4a2a60df9d63230d6298e922b3e08c3c1984ac526dcad2"
+    url "https://github.com/OzelotVanilla/jjdev/archive/refs/tags/v0.1.1.0000.tar.gz"
+    sha256 "77d1446b606578bed8cffc2005b28f0b06d416c0a5cedd6469b4fc93588bb559"
     license "GPL-3.0"
   
     depends_on "jj"
